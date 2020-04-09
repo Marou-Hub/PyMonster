1c8646328b6195d9c1019a43919583f541641697
SMN <thetarget@online.fr>
2020-04-09 08:46:16 +0200

    Sky kivy start playground

--- sky_kivy/main.py
+++ sky_kivy/main.py
@@ -2,18 +2,22 @@ from platform import python_version
 import kivy
 kivy.require('1.9.1')
 
+from kivy.graphics import Color, Rectangle
+from kivy.properties import DictProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.treeview import TreeView, TreeViewLabel, TreeViewNode
+from kivy.uix.treeview import TreeView,  TreeViewNode
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window, platform
 from os.path import join, dirname
+from random import random
 
 # KIVY LAUNCHER uses KIVY 1.9.1 and PYTHON 2.7.2 !!
 # Things that DO NOT WORK with KIVY LAUNCHER (but work on desktop)
@@ -108,16 +112,85 @@ class Menu(Screen):
         layout.add_widget(buttons)
         tv = TreeView(hide_root=True, size_hint_y=None)
         tv.bind(minimum_height=tv.setter('height'))
-        for i in range(20):
+        max_level = 1
+        for i in range(1, 20):
             tvb = TreeViewButton()
-            tvb.build('Level ' + str(i), i>3)
-            #tv.add_node(TreeViewLabel(text='[color=ff0000][size=60]Level ' + str(i) + '[/size][/color]', markup=True, halign='center', size_hint=(1, .4)))
+            tvb.build('Level ' + str(i), i > max_level)
             tv.add_node(tvb)
         sv = ScrollView(bar_width=50, do_scroll_x=False)
-        #sv = ScrollView(size_hint=(1, None), size=(Window.width, Window.height), bar_width=50, do_scroll_x=False)
         sv.add_widget(tv)
         layout.add_widget(sv)
         self.add_widget(layout)
+
+
+class Playground(Widget):
+
+    cbounds = ListProperty([])
+    cmap = DictProperty({})
+    blist = ListProperty([])
+
+    def __init__(self, **kwargs):
+        self._hue = 0
+        super(Playground, self).__init__(**kwargs)
+        self.init_physics()
+        self.bind(size=self.update_bounds, pos=self.update_bounds)
+        self.texture = Image(source=join(curdir, 'images', 'circle.png'), mipmap=True).texture
+        Clock.schedule_interval(self.step, 1 / 30.)
+
+    def init_physics(self):
+        # create the space for physics simulation
+        pass
+
+    def step(self, dt):
+        self.space.step(1 / 30.)
+        self.update_objects()
+
+    def update_objects(self):
+        for body, obj in self.cmap.iteritems():
+            p = body.position
+            radius, color, rect = obj
+            rect.pos = p.x - radius, p.y - radius
+            rect.size = radius * 2, radius * 2
+
+    def add_random_circle(self):
+        self.add_circle(
+            self.x + random() * self.width,
+            self.y + random() * self.height,
+            10 + random() * 50)
+
+    def add_circle(self, x, y, radius):
+        # create a falling circle
+        body = cy.Body(100, 1e9)
+        body.position = x, y
+        circle = cy.Circle(body, radius)
+        circle.elasticity = 0.6
+        #circle.friction = 1.0
+        self.space.add(body, circle)
+
+        with self.canvas.before:
+            self._hue = (self._hue + 0.01) % 1
+            color = Color(self._hue, 1, 1, mode='hsv')
+            rect = Rectangle(
+                texture=self.texture,
+                pos=(self.x - radius, self.y - radius),
+                size=(radius * 2, radius * 2))
+        self.cmap[body] = (radius, color, rect)
+
+        # remove the oldest one
+        self.blist.append((body, circle))
+        if len(self.blist) > 200:
+            body, circle = self.blist.pop(0)
+            self.space.remove(body)
+            self.space.remove(circle)
+            radius, color, rect = self.cmap.pop(body)
+            self.canvas.before.remove(color)
+            self.canvas.before.remove(rect)
+
+    def on_touch_down(self, touch):
+        self.add_circle(touch.x, touch.y, 10 + random() * 20)
+
+    def on_touch_move(self, touch):
+        self.add_circle(touch.x, touch.y, 10 + random() * 20)
 
 
 sm = ScreenManager()
